287e09851e91dabdcf5561535743c803e0ba2688
Owen Colegrove <owen@emergentagi.org>
2024-01-03 23:57:07 +0000

    messing w/ sphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -69,3 +69,6 @@ html_css_files = [
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+
+def setup(app):
+    app.add_css_file('custom.css')
